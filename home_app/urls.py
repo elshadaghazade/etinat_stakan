fa4e1ea8c843d2ff2d627dbeac15f1326dd8f1e2
user1 <user1@MacBook-Pro-Mac.local>
2021-05-29 02:36:55 +0400

    verifyzohomail

--- home_app/urls.py
+++ home_app/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home_view, name='homepage'),
-    path('verifyforzoho.html', verify_zoho_view, name='verifyzoho')
+    path('zohoverify/verifyforzoho.html', verify_zoho_view, name='verifyzoho')
 ]
